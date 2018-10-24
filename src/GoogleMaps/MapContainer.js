d326471c781e79de477586628964aca98b6be14f
John Cooley <johndavidcooley@gmail.com>
2018-10-24 17:49:49 -0400

    Getting started with Google Maps using the google-maps-react npm package.

---
+++ src/GoogleMaps/MapContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+export default class MapContainer extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+ 
