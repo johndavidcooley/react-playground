32e633016bc51a6c43ce306dc4a00900a0a97889
John Cooley <johndavidcooley@gmail.com>
2018-10-25 23:31:52 -0400

    Well, I managed to get Google Maps loaded, basically by copying from the google-maps-react docs. That's cool and all, but I need to get my own API key before continuing any development there. I'll clean this up soon.

--- src/GoogleMaps/GoogleMaps.js
+++ src/GoogleMaps/GoogleMaps.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
+import MapContainer from './MapContainer';
+
 export default class GoogleMaps extends Component {
   render() {
     return (
       <div className="google-maps">
         Google Maps
+        <MapContainer /> 
       </div>
     )
   }
--- src/GoogleMaps/MapContainer.js
+++ src/GoogleMaps/MapContainer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-export default class MapContainer extends Component {
+export class MapContainer extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Map google={this.props.google} zoom={14}>
+        <Marker name={'Current location'} />
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              {/* <h1>{this.state.selectedPlace.name}</h1> */}
+            </div>
+        </InfoWindow>
+      </Map>
     )
   }
 }
+
+export default GoogleApiWrapper({
+  // apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
+})(MapContainer)
  
